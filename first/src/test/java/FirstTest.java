ad704941ebdf0fbfce1a658ee7c687bcc292de3e
Andy Goossens <andy.goossens@eurid.eu>
2016-04-06 12:15:10 +0200

    Initial commit

---
+++ first/src/test/java/FirstTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class FirstTest {
+
+  @Test
+  public void testFoo() throws Exception {
+    // no-op
+  }
+
+  public static void something() {
+    // no-op
+  }
+}
---
+++ second/src/test/java/SecondTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class SecondTest {
+
+  @Test
+  public void testBar() throws Exception {
+    FirstTest.something();
+  }
+}
